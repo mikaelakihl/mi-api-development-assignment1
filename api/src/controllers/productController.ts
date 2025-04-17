1899e0b02c1d7ee2a8c0609b344629757e0aaa8d
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-04-17 11:04:05 +0200

    feat: added CRUD: getProductsById

--- api/src/controllers/productController.ts
+++ api/src/controllers/productController.ts
@@ -11,3 +11,21 @@ export const getAllProducts = async (req: Request, res: Response) => {
         res.status(500).send('Error fetching products');
     }
 };
+
+export const getProductById = async (req: Request, res: Response) => {
+    const productId = req.params.id;
+
+    try {
+        const [rows] = await db.query('SELECT * FROM products WHERE id = ?', [productId]);
+    
+        if ((rows as any[]).length === 0) {
+            return res.status(404).send('Product not found');
+        }
+
+        res.json(rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching product by id');
+    }
+
+};
--- api/src/routes/productRoute.ts
+++ api/src/routes/productRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllProducts } from "../controllers/productController";
+import { getAllProducts, getProductById } from "../controllers/productController";
 
 const router = express.Router();
 
 router.get('/products',getAllProducts);
+router.get('/products/:id', getProductById);
 
 export default router;
