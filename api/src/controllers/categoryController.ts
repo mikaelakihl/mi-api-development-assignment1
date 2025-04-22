5c2e5327309b6f5b1f91ce0c37c237fa576815b0
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-04-22 11:34:51 +0200

    feat: added CRUD: getProductsByCategoryId

--- api/src/controllers/categoryController.ts
+++ api/src/controllers/categoryController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { json, Request, Response } from 'express';
 import { db } from '../config/db';
 
 
@@ -11,3 +11,28 @@ export const getAllCategories = async (req: Request, res: Response) => {
         res.status(500).send('Error fetching categories');
     }
 };
+
+export const getProductsByCategoryId = async (req: Request, res: Response) => {
+    const categoryId = req.params.id;
+
+    try {
+        const [rows] = await db.query(`
+            SELECT products.*, categories.name AS category_name
+            FROM products
+            JOIN categories ON products.category_id = categories.id
+            WHERE category_id = ?
+            `, [categoryId]);
+        
+        if ((rows as any []).length === 0) {
+            return res.status(404).send('No products found for this category');
+        }    
+
+        res.json(rows);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching products by category');
+    }
+};
+
+        
--- api/src/routes/categoryRoute.ts
+++ api/src/routes/categoryRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllCategories } from "../controllers/categoryController";
+import { getAllCategories, getProductsByCategoryId } from "../controllers/categoryController";
 
 const router = express.Router();
 
 router.get('/categories',getAllCategories);
+router.get('/categories/:id/products',getProductsByCategoryId);
 
 export default router;
